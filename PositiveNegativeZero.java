b2982ceb75391e76e07535c887d1325a5dbb2bbb
Youssef <youssefouhti@gmail.com>
2026-03-29 20:59:33 +0200

    "1.2"

--- PositiveNegativeZero.java
+++ PositiveNegativeZero.java
@@ -17,6 +17,16 @@ import java.util.Scanner;
         
 // }   
 
+// public class PositiveNegativeZero {
+//     // write code here
+//     public static void checkNumber ( int number){
+        
+//         System.out.println(number>0?"positive":(number<0 ? "negative": "zero"));
+//     }
+    
+// }
+// the expression (condition ? value_if_true : value_if_false) is a ternary operator that evaluates the condition and returns one of two values based on whether the condition is true or false. In this case, it checks if the number is greater than 0, less than 0, or equal to 0 and returns the corresponding string ("positive", "negative", or "zero") accordingly.
+
 public class PositiveNegativeZero {
     public static void main(String[] args) {
         double number = getNumber();
